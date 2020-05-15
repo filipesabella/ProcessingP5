56a4a3bdb51c7d1bbd865e91d4b277a87ba31990
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-15 20:17:00 +0000

    Move initialisation stuff inside App

--- src/main/components/App.tsx
+++ src/main/components/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { useEffect } from 'react';
 import { initEditor } from './Editor';
+import { openPreviewWindow } from './PreviewWindow';
 
 require('../styles/app.less');
 
 export const App = () => {
   useEffect(() => {
     initEditor();
+    openPreviewWindow();
   });
 
   return <div className='app'></div>;
