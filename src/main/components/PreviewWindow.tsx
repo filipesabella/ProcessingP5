384fe8ab4c564d296ce85a78e598b54d37d04ea5
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-15 20:07:00 +0000

    Store site and position state for preview window

--- src/main/components/PreviewWindow.tsx
+++ src/main/components/PreviewWindow.tsx
@@ -1,26 +1,13 @@
 import { BrowserWindow } from 'electron';
 import { currentSketchFiles, p5Path, readIndexTemplate } from '../lib/file-system';
 
-const remote = window.require('electron').remote;
+const windowStateKeeper = window.require('electron-window-state');
+const electron = window.require('electron');
+const remote = electron.remote;
 
 export function openPreviewWindow() {
   // close any current open windows
   applyToPreviewWindow(w => !w.isDestroyed() && w.close());
-
-  const win = new remote.BrowserWindow({
-    parent: remote.getCurrentWindow(),
-    modal: false,
-    x: 1500,
-    y: 500,
-    height: 1000,
-    titleBarStyle: 'hiddenInset',
-    autoHideMenuBar: true,
-    closable: false, // does not work
-    webPreferences: {
-      webSecurity: false,
-      nodeIntegration: true,
-    },
-  }) as BrowserWindow;
 
   const scripts = [p5Path()].concat(currentSketchFiles())
     .map(s => `<script src="${s}"></script>`)
@@ -29,7 +16,46 @@ export function openPreviewWindow() {
   const src = readIndexTemplate().replace('$scripts', scripts);
   const file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(src);
 
+  const win = buildBrowserWindow();
   win.loadURL(file);
+}
+
+export function reloadPreviewWindow(): void {
+  // not allowing the preview window to be closed just wasn't working with
+  // Electron, so we added this garbage here instead.
+  if (remote.BrowserWindow.getAllWindows().length === 1) {
+    openPreviewWindow();
+  } else {
+    applyToPreviewWindow(w => w.reload());
+  }
+}
+
+function buildBrowserWindow(): BrowserWindow {
+  const windowState = windowStateKeeper({
+    x: window.screen.availWidth / 2,
+    y: 0,
+    defaultWidth: window.screen.availWidth / 2,
+    defaultHeight: window.screen.availHeight,
+  });
+
+  const win = new remote.BrowserWindow({
+    file: 'previewWindow.json',
+    parent: remote.getCurrentWindow(),
+    modal: false,
+    titleBarStyle: 'hiddenInset',
+    autoHideMenuBar: true,
+    closable: false, // does not work
+    x: windowState.x,
+    y: windowState.y,
+    width: windowState.width,
+    height: windowState.height,
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: true,
+    },
+  }) as BrowserWindow;
+
+  windowState.manage(win);
 
   win.on('resize', () => win.reload());
 
@@ -43,16 +69,8 @@ export function openPreviewWindow() {
       console.error(message);
     }
   });
-}
 
-export function reloadPreviewWindow(): void {
-  // not allowing the preview window to be closed just wasn't working with
-  // Electron, so we added this garbage here instead.
-  if (remote.BrowserWindow.getAllWindows().length === 1) {
-    openPreviewWindow();
-  } else {
-    applyToPreviewWindow(w => w.reload());
-  }
+  return win;
 }
 
 function applyToPreviewWindow(fn: (w: BrowserWindow) => void): void {
