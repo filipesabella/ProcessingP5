08a44c3bd13f40c321068af404357b283e17d963
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-14 22:11:26 -0300

    Add a p5 function to the autocomplete

--- src/main/components/Editor.tsx
+++ src/main/components/Editor.tsx
@@ -11,6 +11,44 @@ import monaco = require('monaco-editor/esm/vs/editor/editor.main.js');
 };
 
 export const initEditor = () => {
+
+  function createDependencyProposals() {
+    // returning a static list of proposals, not even looking at the prefix (filtering is done by the Monaco editor),
+    // here you could do a server side lookup
+    return [{
+      label: 'ellipse',
+      kind: monaco.languages.CompletionItemKind.Function,
+      documentation:
+        `ellipse(x, y, w, h?)
+        ellipse(x, y, w, h, detail)
+
+        Parameters:
+            x Number: x-coordinate of the ellipse.
+            y Number: y-coordinate of the ellipse.
+            w Number: width of the ellipse.
+            h Number: height of the ellipse. (Optional)
+            detail Integer: number of radial sectors to draw (for WebGL mode)
+
+            Draws an ellipse (oval) to the screen. An ellipse with equal width and
+          height is a circle. By default, the first two parameters set the
+          location, and the third and fourth parameters set the shape's width and
+          height. If no height is specified, the value of width is used for both
+          the width and height. If a negative height or width is specified, the
+          absolute value is taken. The origin may be changed with the
+          ellipseMode() function.`,
+      insertText: 'ellipse',
+    }];
+  }
+
+  monaco.languages.registerCompletionItemProvider('javascript', {
+    provideCompletionItems: () => {
+      return {
+        suggestions: createDependencyProposals()
+      };
+    }
+  });
+
+
   // options
   // https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditorconstructionoptions.html
   const editor =
@@ -20,7 +58,9 @@ export const initEditor = () => {
       theme: 'vs-dark',
       folding: false,
       formatOnType: true,
+      formatOnPaste: true,
       highlightActiveIndentGuide: false,
+      lineNumbers: true,
       minimap: {
         enabled: false,
       },
