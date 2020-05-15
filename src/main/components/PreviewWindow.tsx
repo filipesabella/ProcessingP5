dbc274adf164d82538beef18d2e3733828cf5560
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-15 17:13:16 -0300

    Refactor file stuff out

--- src/main/components/PreviewWindow.tsx
+++ src/main/components/PreviewWindow.tsx
@@ -1,14 +1,14 @@
 import { BrowserWindow } from 'electron';
+import { currentSketchFiles, p5Path, readIndexTemplate } from '../lib/file-system';
+
+// this is necessary to not crap out importing electron on this render thread
+const remote = window.require('electron').remote;
 
 export function openPreviewWindow() {
-  // this is necessary to not crap out importing electron on this render thread
-  const remote = window.require('electron').remote;
   const BW = remote.BrowserWindow;
 
   // for dev, close all other windows
-  (BW.getAllWindows() as any[]).forEach(w => {
-    if (w !== remote.getCurrentWindow()) w.close();
-  });
+  applyToPreviewWindow(w => w.close());
 
   let win = new BW({
     parent: remote.getCurrentWindow(),
@@ -18,6 +18,7 @@ export function openPreviewWindow() {
     height: 1000,
     titleBarStyle: 'hiddenInset',
     autoHideMenuBar: true,
+    closable: false,
     webPreferences: {
       webSecurity: false,
       nodeIntegration: true,
@@ -27,14 +28,22 @@ export function openPreviewWindow() {
   win.webContents.openDevTools();
   win.on('resize', () => win.reload());
 
-  const fs = remote.require('fs');
-  const basePath = remote.app.getAppPath();
-  const template = fs.readFileSync(`${basePath}/assets/index.html`).toString();
-  const scripts = `
-    <script src="file://${basePath}/assets/p5.js"></script>
-    <script src="file:///home/filipe/ProcessingJS/sketch/main.js"></script>
-  `;
-  const src = template.replace('$scripts', scripts);
+  const scripts = [p5Path()].concat(currentSketchFiles())
+    .map(s => `<script src="${s}"></script>`)
+    .join('\n');
+
+  const src = readIndexTemplate().replace('$scripts', scripts);
   const file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(src);
+
   win.loadURL(file);
+}
+
+export function reloadPreviewWindow(): void {
+  applyToPreviewWindow(w => w.reload());
+}
+
+function applyToPreviewWindow(fn: (w: any) => void): void {
+  (remote.BrowserWindow.getAllWindows() as any[]).forEach(w => {
+    if (w !== remote.getCurrentWindow()) fn(w);
+  });
 }
