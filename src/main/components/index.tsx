53d0c85e003fdc136a27cc891d140ead1f51d7c3
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-15 22:14:56 -0300

    Move initialisation stuff inside App

--- src/main/components/index.tsx
+++ src/main/components/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './App';
-import { openPreviewWindow } from './PreviewWindow';
 
 require('typeface-jetbrains-mono');
 
@@ -10,7 +9,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-window.setTimeout(openPreviewWindow, 500);
 
 // initialise the Monaco editor. this is actually necessary
 (self as any).MonacoEnvironment = {
