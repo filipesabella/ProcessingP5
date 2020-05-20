640774884851ef759c3727f84a55a98dc6688485
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-20 20:23:00 +0000

    Trying to get icons to work on linux

--- ship.sh
+++ ship.sh
@@ -7,15 +7,14 @@ yarn
 ./node_modules/.bin/parcel build node_modules/monaco-editor/esm/vs/editor/editor.worker.js --no-source-maps
 ./node_modules/.bin/parcel build ./src/main/index.html --public-url ./
 
+echo Copying icons
 mkdir -p dist/icons
-cp assets/icons/png/512x512.png dist/icons/icon.png
+cp assets/icons/png/* dist/icons/
 cp assets/icons/win/* dist/icons/
 cp assets/icons/mac/* dist/icons/
 
 GH_TOKEN=`cat token` ./node_modules/.bin/electron-builder build \
-  --mac dmg \
   --linux AppImage deb \
-  --win \
-  --publish always
+  # --publish always
 
 # ./node_modules/.bin/electron-builder build -m
