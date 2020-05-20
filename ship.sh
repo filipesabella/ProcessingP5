98c8bda85005715ec1c9587f3a7ca92a3ec57e34
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-20 20:21:00 +0000

    Packaging stuff

--- ship.sh
+++ ship.sh
@@ -1,13 +1,16 @@
+rm -rf dist/ .cache/ node_modules
+yarn
+
 ./node_modules/.bin/electron-builder install-app-deps
 
+./node_modules/.bin/parcel build node_modules/monaco-editor/esm/vs/language/typescript/ts.worker.js --no-source-maps
+./node_modules/.bin/parcel build node_modules/monaco-editor/esm/vs/editor/editor.worker.js --no-source-maps
+./node_modules/.bin/parcel build ./src/main/index.html --public-url ./
 
-./node_modules/.bin/parcel build -d build/ node_modules/monaco-editor/esm/vs/language/typescript/ts.worker.js --no-source-maps
-./node_modules/.bin/parcel build -d build/ node_modules/monaco-editor/esm/vs/editor/editor.worker.js --no-source-maps
-./node_modules/.bin/parcel build ./src/main/index.html -d build/ --public-url ./
-
-mkdir -p build/icons
-cp assets/icons/png/512x512.png build/icons/icon.png
-cp assets/icons/win/* build/icons/
-cp assets/icons/mac/* build/icons/
+mkdir -p dist/icons
+cp assets/icons/png/512x512.png dist/icons/icon.png
+cp assets/icons/win/* dist/icons/
+cp assets/icons/mac/* dist/icons/
 
 GH_TOKEN=`cat token` ./node_modules/.bin/electron-builder build -mwl -p always
+# ./node_modules/.bin/electron-builder build -m
