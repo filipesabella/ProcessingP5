ee603d2750efcd392c2b57d7000a54b515092061
Filipe Sabella <filipe.sabella@gmail.com>
2020-05-21 21:58:37 -0300

    Hopefully releasing works now

--- ship.sh
+++ ship.sh
@@ -7,8 +7,15 @@ yarn
 ./node_modules/.bin/parcel build node_modules/monaco-editor/esm/vs/editor/editor.worker.js --no-source-maps
 ./node_modules/.bin/parcel build ./src/main/index.html --public-url ./
 
+echo Copying icons
+mkdir -p dist/icons
+cp assets/icons/png/* dist/icons/
+cp assets/icons/win/* dist/icons/
+cp assets/icons/mac/* dist/icons/
+
 DEBUG=electron-builder GH_TOKEN=`cat token` \
   ./node_modules/.bin/electron-builder build \
   --linux AppImage deb \
   --mac \
-  --win
+  --win \
+  --publish always
